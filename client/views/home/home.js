0e31abb79835b765f8f4af46791c12b2c4941407
myst1349 <secondlife1349@gmail.com>
2015-06-22 17:00:00 +0300

    added id generation for rooms

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -3,9 +3,15 @@
 Session.setDefault('counter', 0);
 
 Template.home.helpers({
+
+  urlId: function () {
+    return Math.random().toString(36).substr(2, 9);
+  },
+
   counter: function () {
     return Session.get('counter');
   }
+
 });
 
 Template.home.events({
